45f1e0c1cab240b95d40d600069e4b0d23a7c76d
ANDA LIU <64123020+851687992@users.noreply.github.com>
2026-07-11 22:53:57 +0800

    fix: use Tampermonkey JavaScript sandbox

--- boolean-ops.user.js
+++ boolean-ops.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Draw.io Boolean Operations
 // @namespace    https://github.com/851687992/drawio-boolean-ops
-// @version      0.1.2
+// @version      0.1.3
 // @description  Adds Union, Subtract, Intersect, and Exclude to app.diagrams.net.
 // @author       ANDA LIU
 // @match        https://app.diagrams.net/*
 // @require      https://cdn.jsdelivr.net/npm/paper@0.12.18/dist/paper-full.min.js
-// @require      https://851687992.github.io/drawio-boolean-ops/boolean-ops.js?v=0.1.2
+// @require      https://851687992.github.io/drawio-boolean-ops/boolean-ops.js?v=0.1.3
 // @run-at       document-start
-// @grant        none
-// @sandbox      MAIN_WORLD
+// @grant        unsafeWindow
+// @sandbox      JavaScript
 // @noframes
 // ==/UserScript==
